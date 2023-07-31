d8044257aa1cd574f958ccc99fd700d8cd522df4
dohyunk58 <140884975+dohyunk58@users.noreply.github.com>
2023-08-01 00:40:23 +0900

    Use rstrip & lstrip

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.py
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.py
@@ -1,13 +1,6 @@
 from sys import stdin
-sstr = stdin.readline().rstrip()
-iblankcount = 0
-if len(sstr) > 0:
-    if sstr[0] == ' ':  # 맨 앞 공백 카운트
-        iblankcount += 1
-    if sstr[-1] == ' ':  # 맨 뒤 공백 카운트
-        iblankcount += 1
-    if sstr == ' ':
-        iblankcount += 1  # 빈칸만 존재하는 경우 0 + 1 - 1로 만들어줌
-    print(sstr.count(' ')+1-iblankcount)
-else:
+sstr = stdin.readline().rstrip().lstrip()  # rstrip과 lstrip은 공백 기준 우측, 좌측 제거
+if sstr == '':
     print(0)
+else:
+    print(sstr.count(' ')+1)
