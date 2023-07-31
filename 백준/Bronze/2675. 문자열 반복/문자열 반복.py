decbfd21276379939768b72cf52a22b2c285b87a
dohyunk58 <140884975+dohyunk58@users.noreply.github.com>
2023-07-31 23:26:28 +0900

    [Bronze II] Title: 문자열 반복, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -1,9 +1,7 @@
 from sys import stdin
 inum = int(stdin.readline())
 for i in range(inum):
-    slist = []
     irepeatnum, sstr = stdin.readline().split()
     for j in sstr:
-        for k in range(int(irepeatnum)):
-            slist.append(j)
-    print(*slist, sep='')
+        print(j*int(irepeatnum), end='')
+    print('')
