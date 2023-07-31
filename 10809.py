ffb4dafb302989903a95a8c7b020cfe38c0a20d8
dohyunk58 <hedge3x@gmail.com>
2023-07-31 22:35:34 +0900

    second commit

--- 10809.py
+++ 10809.py
@@ -1,7 +1,7 @@
 # 입력받은 문자열의 알파벳이 몇 번째 칸에 있는지 리스트에 저장 후 출력
 import sys
 alpha_list = [-1]*123  # 알파벳 소문자 아스키코드 97 ~ 122를 위한 배열 생성
-S = sys.stdin.readline().rstrip()
+S = sys.stdin.readline().rstrip()  # rstrip으로 개행문자 제거
 setS = set(S)  # 문자열 중복 제거
 for i in range(len(setS)):
     alpha_list[ord(list(setS)[i])] = S.index(
