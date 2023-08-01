6adba874499d7486c8be34a34a5b2f582a3805fa
dohyunk58 <140884975+dohyunk58@users.noreply.github.com>
2023-08-02 00:49:11 +0900

    Add comment

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.py
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.py
@@ -1,9 +1,9 @@
 import sys
 
-lstr = []
+lstr = [] 
 while True:
     sinput = sys.stdin.readline().rstrip()
-    if sinput == '':
+    if sinput == '': # 빈칸일 경우 입력 종료
         break
     else:
         lstr.append(sinput)
