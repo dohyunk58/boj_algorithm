72766c3357f303692c0f1b1769990783fbce4d1b
dohyunk58 <140884975+dohyunk58@users.noreply.github.com>
2023-08-02 00:01:33 +0900

    Use ASCII
    아스키코드를 이용해서 다이얼을 구함

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,21 +1,14 @@
 from sys import stdin
 
-count = 0  # 다이얼 이동 시간 카운트 (1=2초, 2=3초 ... 9=10초)
+numcount = 0  # 다이얼 이동 시간 카운트 (1=2초, 2=3초 ... 9=10초)
+sstr = stdin.readline().rstrip()
 
-ddial = {'A': 2, 'B': 2, 'C': 2,
-         'D': 3, 'E': 3, 'F': 3,
-         'G': 4, 'H': 4, 'I': 4,
-         'J': 5, 'K': 5, 'L': 5,
-         'M': 6, 'N': 6, 'O': 6,
-         'P': 7, 'Q': 7, 'R': 7, 'S': 7,
-         'T': 8, 'U': 8, 'V': 8,
-         'W': 9, 'X': 9, 'Y': 9, 'Z': 9}  # 주어진 알파벳을 숫자로 대응(딕셔너리)
+for i in sstr:
+    if i < "S":
+        numcount += int((ord(i)-59)/3+1)  # ord('A') = 65, (65-59)/3 = 2
+    elif i < "Z":
+        numcount += int((ord(i)-60)/3+1)  # 7이 PQRS 4개이기 때문
+    else:
+        numcount += 10  # 문자가 Z인경우
 
-lstr = list(stdin.readline().rstrip())  # 문자열을 한 글자씩 리스트로 만들기
-
-for i in lstr:
-    # print(i)
-    # print(ddial[i])
-    count += ddial[i]+1
-
-print(count)
+print(numcount)
